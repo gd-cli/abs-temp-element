c84debf55a55cd7f828a7c26f9740834939ad496
wakeupmycat <nxp_xinpeng@126.com>
2019-11-06 17:14:57 +0800

    ask.js

---
+++ ask.js
@@ -0,0 +1,24 @@
+module.exports = [
+  {
+    type: 'confirm',
+    name: 'private',
+    message: 'ths resgistery is private?',
+  },
+  {
+    type: 'input',
+    name: 'author',
+    message: 'author?',
+    default: 'goldenstand',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'description?',
+  },
+  {
+    type: 'input',
+    name: 'license',
+    message: 'license?',
+    default: 'ISC',
+  },
+];
